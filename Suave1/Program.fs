d20e5f5318318e32bc08a3a5f8826e120b78e7b7
rustystone <marcush.bouvet@icloud.com>
2017-03-27 16:18:54 +0200

    Add project files.

---
+++ Suave1/Program.fs
@@ -0,0 +1,35 @@
+﻿#light
+
+
+module FsharpApp
+
+open FSharp.Configuration
+
+open Suave
+open Suave.Json
+open System.Runtime.Serialization
+open System.Net
+open System
+
+type Settings = AppSettings<"App.config">
+
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Hello World" 
+    Settings.SuaveIp
+    Settings.SuavePort
+    Settings.EmailPassword
+    
+    Console.WriteLine("Settings Suave IP: {0}", Settings.SuaveIp)
+    let x1 = Settings.SuavePort |> int
+    let int1 = 1
+    let s = x1.GetType().Name
+    if s = "Int32" then
+        Console.WriteLine("Typeof: Settings Suave Port: {0}", (Settings.SuavePort |> int))
+    Console.ReadLine() |> ignore
+    0
+
+
+
---
+++ Suave1/Script2.fsx
@@ -0,0 +1,36 @@
+﻿#r "C:\\PCode2\\Suave1\\Suave1\\packages\\FSharp.Configuration.0.6.4\\lib\\net40\\FSharp.Configuration.dll"
+#r "System.Configuration.dll"
+open FSharp.Configuration
+
+#r "C:\\PCode2\\Suave1\\Suave1\\packages\\Suave.2.0.5\\lib\\net40\\Suave.dll"
+open Suave
+#r "System.Runtime.Serialization.dll"
+open Suave.Json
+open System.Runtime.Serialization
+open System.Net
+open System
+
+type Settings = AppSettings<"App.config">
+
+Settings.SuaveIp
+Settings.SuavePort
+Settings.EmailPassword
+
+
+[<DataContract>]
+type Foo =
+    {[<field: DataMember(Name = "foo")>]
+        foo: string}
+
+[<DataContract>]
+type Bar =
+    {[<field: DataMember(Name = "bar")>]
+        bar: string}
+
+let cfg =
+  { defaultConfig with
+      bindings =
+        [ HttpBinding.create HTTP IPAddress.Loopback 80us
+          HttpBinding.createSimple HTTP Settings.SuaveIp (Settings.SuavePort |> int) ]}
+
+startWebServer cfg (mapJson (fun (a:Foo) -> {bar = a.foo}))
